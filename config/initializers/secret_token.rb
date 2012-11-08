65e59cc75414c58238aaad30e09b9c4b38616957
Tyler Rick <tyler@k3integrations.com>
2012-11-07 17:21:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlHelpersInDecoratorUsedInMailerView::Application.config.secret_token = '010111d109b7cc4b0c4c456bd1dbbce49fdad6c834cecfa36ed26afa1ef6403d3ab649bc26ca73016bf3c6b17317c532686e3109a22f9e31525658d95d57c1a6'
